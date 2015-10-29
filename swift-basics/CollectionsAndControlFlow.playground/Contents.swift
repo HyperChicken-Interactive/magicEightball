ae96e9735a641cf7befe193e0fda9b8822beaeb3
Whitman Huntley <huntleywhitman@gmail.com>
2015-10-29 18:08:47 -0400

    Swift arrays

---
+++ swift-basics/CollectionsAndControlFlow.playground/Contents.swift
@@ -0,0 +1,17 @@
+    //Arrays
+
+var todo: [String] = ["Finish Collections course",
+    "buy groceries",
+    "resond to emails"]
+
+todo.append("Pick Up Drycleaning.")
+
+todo += ["Order Book online", "Learn GO!"]
+
+let greetings: [String] = ["Well hello there!", "How're you?", "What's Up?"]
+
+//greetings.append("Hey there!")
+//Doesn't work, greetings is constant
+
+//Reading From Arrays
+
