8029de325de15d30f4aaf2a8dd0fcb4c8692fa40
Whitman Huntley <huntleywhitman@gmail.com>
2016-02-17 14:13:31 -0500

    save

--- Object Oriented swift/Object Oriented Swift.playground/Pages/Classes.xcplaygroundpage/Contents.swift
+++ Object Oriented swift/Object Oriented Swift.playground/Pages/Classes.xcplaygroundpage/Contents.swift
@@ -23,13 +23,13 @@ struct Point {
 //Now to the real code:
 
 class Enemy{
-    var hp: Int = 2
+    var hp: Int = 1
     var position: Point
     init(x: Int, y: Int){
         self.position = Point(x: x, y: y)
     }
-    func decreaseHealth(factor: Int){
-        hp -= factor
+    func decreaseHealth(factor by: Int){
+        hp -= by
     }
 }
 
@@ -41,22 +41,57 @@ class Tower{
         self.position = Point(x: x, y: y)
         self.range = range
     }
-    func fireAtEnemy(enemy:Enemy){
+    func fireAtEnemy(enemy e: Enemy){
+        if testForInRange(position: self.position, range: self.range, target: e.position){
+            while e.hp > 0{
+                e.decreaseHealth(factor: self.attk)
+                print("booyah! The tower killed it")
+            }
+        } else {
+            print("Damn, it got away")
+        }
         
     }
     func testForInRange(position p:Point, range r:Int, target t:Point) ->Bool{
         let availablePositions = position.surroundingPoints(withRange: range)
-        for p in availablePositions {
-            if (p.x == t.x) && (p.x == p.y){
+        for point in availablePositions {
+            if (point.x == t.x) && (point.y == t.y){
                 return true
             }
+        }
+    return false
+    }
+}
+
+class SuperEnemy: Enemy {
+    override init(x: Int, y: Int) {
+        super.init(x: x, y: y)
+        self.hp = 5
+    }
+}
+
+class LaserTower: Tower {
+    override init(x: Int, y: Int, range: Int) {
+        super.init(x: x, y: y, range: range)
+        self.range = 100
+        self.attk = 50
+    }
+    override func fireAtEnemy(enemy e: Enemy) {
+        while e.hp > 0{
+            e.decreaseHealth(factor: self.attk)
+            print("booyah! The tower killed it")
         }
     }
 }
 
+let tower:Tower = Tower(x: 0, y: 0, range: 1)
+let enemy:Enemy = Enemy(x: 1, y: 1)
+
+let sEnemy:SuperEnemy = SuperEnemy(x: 0, y: 0)
+let lTower:LaserTower = LaserTower(x: 500, y: 100, range: 100)
 
 
-
+tower.fireAtEnemy(enemy: enemy)
 
 
 
